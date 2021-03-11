c1f8c514ce9d52a8225d023bc7d26afa19d8a1c4
Hnxind712 <939884922@qq.com>
2021-03-11 18:16:06 +0800

    代码提交.

--- HYEX_Swift/HYEX_Swift/Tool/LSAliyunOSSManager.swift
+++ HYEX_Swift/HYEX_Swift/Tool/LSAliyunOSSManager.swift
@@ -8,13 +8,18 @@
 import Foundation
 import DryAliyunOSS_iOS
 
+
+let AliyunEndPoint = "https://oss-cn-shenzhen.aliyuncs.com";
+let AliyunAccessKey = "LTAI4Fni8NjLwUgjLNM7qevA";
+let AliyunSecretKey = "0qZgTl1kCQQHgTiAd3WrcKFA61Ek9M";
+
 class LSAliyunOSSManager {
     func uploadImage(data image:Data) {
-        DryAliyunOSS.initClient(withEndPoint: "", delegate: self, config: nil)
+        DryAliyunOSS.initClient(withEndPoint: AliyunEndPoint, delegate: self, config: nil)
     }
 }
 extension LSAliyunOSSManager: DryAliyunOSSDelegate{
     func aliyunOSSToken(_ resp: @escaping (String?, String?, String?, String?) -> Void) {
-        
+        resp(AliyunAccessKey,AliyunSecretKey,nil,nil)
     }
 }
